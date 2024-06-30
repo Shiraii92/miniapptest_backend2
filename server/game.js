cb5f15e890cfdbc52c218724fc14503fd5c752ff
Shiraii92 <alexanderlippmann@hotmail.de>
2024-07-01 00:12:36 +0200

    bugs fixed

--- server/game.js
+++ server/game.js
@@ -21,10 +21,10 @@ async function getCurrentRound() {
     try {
         var gameId = await Game.getCurrentGameId();
         var round = await Round.getCurrentRound(gameId);
-        return round || { roundId: null, endAt: null }; // Standardwert setzen, falls keine Runde gefunden wird
+        return round || { roundId: 1, endAt: 2 }; // Standardwert setzen, falls keine Runde gefunden wird
     } catch (error) {
         console.error('Error in getCurrentRound:', error);
-        return { roundId: null, endAt: null }; // Standardwert bei Fehler
+        return { roundId: 1, endAt: 2 }; // Standardwert bei Fehler
     }
 }
 
